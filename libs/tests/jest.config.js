40fefd49e57b12810ed13aaaf1de5b04879cff2a
snilli <mklx1319@gmail.com>
2021-03-22 00:25:59 +0700

    init

---
+++ libs/tests/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  setupFiles: [
+    'reflect-metadata',
+  ],
+  name: 'libs',
+  displayName: 'libs',
+  rootDir: './../../',
+  testMatch: [
+    '<rootDir>/libs/tests/**/*.test.ts',
+  ],
+}
