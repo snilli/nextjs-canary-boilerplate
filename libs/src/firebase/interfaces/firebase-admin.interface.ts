36bcc8079183af0eb2e320c42b513035f4679601
snilli <mklx1319@gmail.com>
2022-01-28 09:45:22 +0700

    change to tailwind

--- libs/src/firebase/interfaces/firebase-admin.interface.ts
+++
@@ -1,9 +0,0 @@
-import { firestore } from 'firebase-admin/lib/firestore'
-import { auth } from 'firebase-admin/lib/auth'
-import Firestore = firestore.Firestore
-import Auth = auth.Auth
-
-export interface FirebaseAdminInterface {
-  firestore(): Firestore
-  auth(): Auth
-}
